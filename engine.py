31a95d8d83c7907473bf5638a1fdf0a0dca84cbd
GregLeland <47698262+GregLeland@users.noreply.github.com>
2019-07-16 22:53:42 -0700

    Add files via upload

--- engine.py
+++ engine.py
@@ -3,10 +3,14 @@ import pandas as pd
 import numpy as np
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import linear_kernel
+from sqlalchemy import create_engine
+import pymysql
+
+engine = create_engine("mysql+pymysql://clfys5mqr32m42ct:p88c2ksec0jals84@rtzsaka6vivj2zp1.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/uyoymimfmvofh7fq")
+query = "SELECT * FROM moovees"
 
 # The 'stars' of the show - the movies themselves!
-movies = pd.read_csv('moovees.csv', encoding='latin-1')
-
+movies = pd.read_sql_query(query, engine)
 # Break up the big genre string into a string array
 movies['genres'] = movies['genres'].str.split('|')
 # Convert genres to string value
